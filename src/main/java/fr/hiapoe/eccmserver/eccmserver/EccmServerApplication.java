93d81396837d7579ec6c3ab5e25ba8ab3d533567
nicomahery <nicomahery@gmail.com>
2022-04-12 10:35:24 +0200

    Initial commit

---
+++ src/main/java/fr/hiapoe/eccmserver/eccmserver/EccmServerApplication.java
@@ -0,0 +1,13 @@
+package fr.hiapoe.eccmserver.eccmserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EccmServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EccmServerApplication.class, args);
+    }
+
+}
